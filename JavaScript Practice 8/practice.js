ac22b9ff225543232d0e5945dd7e090351a33ad0
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-08-12 12:19:27 +0600

    Practicing === operations

--- JavaScript Practice 8/practice.js
+++ JavaScript Practice 8/practice.js
@@ -11,8 +11,13 @@ let price2 = 20;
 // }else{
 //     console.log ("price1 is not greater than price2")
 // }
-if (price1 == price2){
-    console.log("proce1 is equal to pric2");
-}else{
-    console.log("price1 is not equal to price2");
+// if (price1 == price2){
+//     console.log("proce1 is equal to pric2");
+// }else{
+//     console.log("price1 is not equal to price2");
+// }
+if (price1===price2){
+    console.log("proce1 is equal to price 2");
+} else{
+    console.log("price1 in not equal to price2");
 }
