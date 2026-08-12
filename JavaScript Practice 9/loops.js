fca053860ac369033d97c786c749a7bd3f0b3a54
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-08-12 22:56:06 +0600

    Task 2 Write a for loop that prints number from 1 to 10

--- JavaScript Practice 9/loops.js
+++ JavaScript Practice 9/loops.js
@@ -7,3 +7,12 @@
 //     console.log("Hello");
 //     i++;
 // }
+// Task 2: Write a for loop that prints numbers from 1 to 10
+// for (let i = 1 ; i <= 10 ; i++){
+//     console.log(i);
+// }
+let i = 0;
+while(i < 10){
+    console.log(i);
+    i++;
+}
