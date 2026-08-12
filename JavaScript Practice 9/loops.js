ff8f44197e671aa5129d0ac496679a27c82923f2
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-08-12 22:48:36 +0600

    Task 1 print hello five time using for and while loop

---
+++ JavaScript Practice 9/loops.js
@@ -0,0 +1,9 @@
+// Task 1: Print "Hello" Five times
+// for (let i = 1; i <= 5 ; i++ ) {
+//     console.log("Hello");
+// }
+// let i = 1;
+// while( i <= 5){
+//     console.log("Hello");
+//     i++;
+// }
