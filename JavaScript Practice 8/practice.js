85947d3e473f48038b384a20c57a8014c6f63a0a
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-08-12 12:10:58 +0600

    Practice JavaScript-Conditional Operator

---
+++ JavaScript Practice 8/practice.js
@@ -0,0 +1,13 @@
+// Take two variables price1 and price2, and compare them using all comparison operators.(==, ===,!=.!==, >,<, >=, <=) and print the results to the console.
+let price1 = 10;
+let price2 = 20;
+// if (price1 < price2) {
+//     console.log("Price 1 is less than price 2");
+// } else {
+//     console.log("Price 1 is not less than price 2");
+// }
+// if (price1 > price2){
+//     console.log("price1 is greater then price2"):
+// }else{
+//     console.log ("price1 is not greater than price2")
+// }
