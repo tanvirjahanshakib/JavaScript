052671843071862babc3cb1ff7584c6f3470d3ae
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-08-21 18:39:53 +0600

    Problem solving FizzBuzz

--- JavaScript Practice 9/loops.js
+++ JavaScript Practice 9/loops.js
@@ -69,9 +69,31 @@
 //     console.log("The Number of Count is", i);
 // }
 // Write a for loop from 1 to 20 that skips any number divisible by 4
-for ( let i = 1 ;  i <= 20 ; i++){
-    if ( i % 4 === 0){
-        continue;
+// for ( let i = 1 ;  i <= 20 ; i++){
+//     if ( i % 4 === 0){
+//         continue;
+//     }
+//     console.log("The Value of i is", i ); 
+// }
+
+// problem 4: problem solving
+
+function fizzBuzz(number) {
+
+    for (let i = 1; i <= number; i++) {
+        console.log("The Value of i is", i);
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        }
+        else if (i % 3 === 0) {
+            console.log("Fizz");
+        }
+        else if (i % 5 === 0) {
+            console.log("Buzz");
+        }
+        else {
+            console.log(i);
+        }
     }
-    console.log("The Value of i is", i ); 
 }
+fizzBuzz(15);
