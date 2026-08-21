6211cd11c4b40e11e76a7cf8512380e7cee5bf8a
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-08-21 20:11:54 +0600

    Problem 1 check Even or Odd number

---
+++ Problem Solving/problem1.js
@@ -0,0 +1,10 @@
+// Check if a number is Even or Odd
+
+function checkEvenOrOdd(number){
+    if(number %2 ===0){
+        return "This is an Even number";
+    } else {
+        return "This is an Odd number";
+    }
+}
+console.log(checkEvenOrOdd(5));
