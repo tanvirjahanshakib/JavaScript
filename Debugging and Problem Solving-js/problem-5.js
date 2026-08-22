4b8c19fdf8c30effab4ddeb8189eff6bece990b0
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-08-23 05:19:09 +0600

    Bug solving problem 6

---
+++ Debugging and Problem Solving-js/problem-5.js
@@ -0,0 +1,5 @@
+function subtract(a, b) {
+  return a - b; 
+}
+ 
+console.log(subtract(10, 3)); 
---
+++ Debugging and Problem Solving-js/problem-6.js
@@ -0,0 +1,12 @@
+
+ 
+function isEmpty(str) {
+  if (str.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+ 
+console.log(isEmpty("")); 
+console.log(isEmpty("Love"));
