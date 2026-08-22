564610533f8db85ac92214263fd15c986d6bcd70
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-08-23 05:33:50 +0600

    Bug fixing problem 9

--- Debugging and Problem Solving-js/problem-9.js
+++ Debugging and Problem Solving-js/problem-9.js
@@ -3,3 +3,41 @@ function celsiusToFahrenheit(celsius) {
 }
  
 console.log(celsiusToFahrenheit(0));
+
+
+function sumArray(numbers) {
+  let total = 0;
+ 
+  for (let i = 0; i < numbers.length; i++) { 
+    total = total + numbers[i];
+  }
+ 
+  return total;
+}
+ 
+console.log(sumArray([1, 2, 3, 4]));
+
+
+
+
+function repeatString(str, times) {
+  let result = "";
+ 
+  for (let i = 0; i < times; i++) {
+    result += str;
+  }
+ 
+  return result;
+}
+ 
+console.log(repeatString("ab", 3)); 
+
+
+
+function updateAge(person, newAge) {
+  person.age = newAge;
+  return person;
+}
+ 
+console.log(updateAge({ name: "Lee", age: 20 }, 21));
+
