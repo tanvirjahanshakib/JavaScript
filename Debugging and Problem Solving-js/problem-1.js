38eefd5eca6748a9ce3f640029768e2a86320c66
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-08-23 04:48:38 +0600

    Bug solving problem 1

---
+++ Debugging and Problem Solving-js/problem-1.js
@@ -0,0 +1,31 @@
+// function sayHi(name) {
+//     console.log("Hi, " + name);
+// }
+
+// sayHi("Mira");
+
+
+// // let word = "hello";
+// console.log(word.toUpperCase());
+
+// function square(num) {
+//   return num * num;
+// }
+ 
+// console.log(square(4));
+
+
+// function sayHi(name) {
+//   console.log("Hi, " + name)
+// }
+// sayHi("Mira");
+ 
+
+// let word = "hello";
+// console.log(word.toUpperCase());
+ 
+
+// function square(num) {
+//   return num * num;
+// }
+// console.log(square(4))
