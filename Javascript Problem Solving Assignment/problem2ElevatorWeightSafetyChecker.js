898a1f00b3d15edc89e96a32597e703e4281cb9b
Md. Tanvir Jahan Shakib <mdtanvirjahanshakib@gmail.com>
2026-08-23 20:20:00 +0600

    problem2ElevatorWeightSafetyChecker

---
+++ Javascript Problem Solving Assignment/problem2ElevatorWeightSafetyChecker.js
@@ -0,0 +1,32 @@
+function isElevatorSafe(weights) {
+    let i = 0;
+    for (i = 0; i < weights.length; i++) {
+        if (typeof weights[i] !== "number") {
+            return "Invalid";
+        }
+    }
+    let sum = 0;
+    for (const num of weights) {
+        sum += num;
+    }
+    if (sum > 400) {
+        return false;
+    } else {
+        return true;
+    }
+
+}
+let elevatorWeights1 = [60, 75, 50]
+let elevatorWeights2 = [90, 100, 95, 120]
+let elevatorWeights3 = [400]
+let elevatorWeights4 = "60,75,50"
+console.log(isElevatorSafe(elevatorWeights1));
+console.log(isElevatorSafe(elevatorWeights2));
+console.log(isElevatorSafe(elevatorWeights3));
+console.log(isElevatorSafe(elevatorWeights4));
+
+
+
+
+
+
